8dbd396e3b4afe3ab0a9a4659374e948daebc78a
anik2034 <73274396+anik2034@users.noreply.github.com>
2024-02-17 20:35:41 +0400

    Add Main Fragments (including changes in res) (#3)
    Add Main Fragments

---
+++ app/src/main/java/com/example/capstone/BookListFragment.java
@@ -0,0 +1,47 @@
+package com.example.capstone;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.capstone.databinding.FragmentBookListBinding;
+
+
+public class BookListFragment extends Fragment {
+
+	private static final String ARG_TITLE_ID = "TITLE_ID";
+
+    private FragmentBookListBinding fragmentBookListBinding;
+
+	public static BookListFragment newInstance(int titleID) {
+		Bundle args = new Bundle();
+		args.putInt(ARG_TITLE_ID, titleID);
+		BookListFragment fragment = new BookListFragment();
+		fragment.setArguments(args);
+		return fragment;
+	}
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        fragmentBookListBinding = FragmentBookListBinding.inflate(inflater, container, false);
+        return fragmentBookListBinding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+		Bundle bundle = getArguments();
+		if (bundle != null && bundle.containsKey(ARG_TITLE_ID)) {
+			int titleID = bundle.getInt(ARG_TITLE_ID);
+	        setText(titleID);
+		}
+    }
+
+    public void setText(int id) {
+        if (fragmentBookListBinding != null) {
+            fragmentBookListBinding.setTitle(id);
+        }
+    }
+}
--- app/src/main/java/com/example/capstone/MainActivity.java
+++ app/src/main/java/com/example/capstone/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.capstone;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.capstone.databinding.ActivityMainBinding;
+
+
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(BookListFragment.newInstance(R.string.home_fragment));
+        binding.bottomNavigationView.setBackground(null);
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.home) {
+                replaceFragment(BookListFragment.newInstance(R.string.home_fragment));
+            } else if (itemId == R.id.statistics) {
+                replaceFragment(new StatisticsFragment());
+            } else if (itemId == R.id.settings) {
+                replaceFragment(new SettingsFragment());
+            } else if (itemId == R.id.wishlist) {
+                replaceFragment(BookListFragment.newInstance(R.string.wishlist_fragment));
+            }
+            return true;
+        });
+        binding.addNewBook.setOnClickListener(view -> replaceFragment(new AddNewBookFragment()));
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
     }
 }

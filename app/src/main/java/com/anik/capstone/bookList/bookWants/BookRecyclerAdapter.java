93e46e9573c6150c033f38dd7c371fe9e9851090
Ani <ani_tovmasyan@edu.aua.am>
2024-03-09 13:48:37 +0400

    Create BookGridItem for RecyclerView Add button for switching between grid and linear layouts Add switching functionality to RecyclerView

--- app/src/main/java/com/anik/capstone/bookList/bookWants/BookRecyclerAdapter.java
+++ app/src/main/java/com/anik/capstone/bookList/bookWants/BookRecyclerAdapter.java
@@ -1,5 +1,8 @@
 package com.anik.capstone.bookList.bookWants;
 
+import static com.anik.capstone.bookList.ItemViewType.GRID;
+import static com.anik.capstone.bookList.ItemViewType.ROW;
+
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
@@ -8,11 +11,12 @@ import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.ListAdapter;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.anik.capstone.bookList.ItemViewType;
+import com.anik.capstone.databinding.BookGridItemBinding;
 import com.anik.capstone.databinding.BookRowItemBinding;
 import com.anik.capstone.model.BookModel;
 
-
-public class BookRecyclerAdapter extends ListAdapter<BookModel, BookRecyclerAdapter.BookViewHolder> {
+public class BookRecyclerAdapter extends ListAdapter<BookModel, RecyclerView.ViewHolder> {
     private static final DiffUtil.ItemCallback<BookModel> DIFF_CALLBACK = new DiffUtil.ItemCallback<BookModel>() {
         @Override
         public boolean areItemsTheSame(@NonNull BookModel oldItem, @NonNull BookModel newItem) {
@@ -24,32 +28,58 @@ public class BookRecyclerAdapter extends ListAdapter<BookModel, BookRecyclerAdap
             return oldItem.equals(newItem);
         }
     };
+    private ItemViewType itemViewType;
 
     public BookRecyclerAdapter() {
         super(DIFF_CALLBACK);
     }
 
-    @NonNull
+    public void setItemView(ItemViewType itemViewType) {
+        this.itemViewType = itemViewType;
+    }
+
     @Override
-    public BookViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
-        BookRowItemBinding bookRowItemBinding = BookRowItemBinding.inflate(layoutInflater, parent, false);
-        return new BookViewHolder(bookRowItemBinding);
+        if (itemViewType == GRID) {
+            BookGridItemBinding bookGridItemBinding = BookGridItemBinding.inflate(layoutInflater, parent, false);
+            return new BookViewHolderGrid(bookGridItemBinding);
+        } else if (itemViewType == ROW) {
+            BookRowItemBinding bookRowItemBinding = BookRowItemBinding.inflate(layoutInflater, parent, false);
+            return new BookViewHolderRow(bookRowItemBinding);
+        }
+        return null;
+
     }
 
     @Override
-    public void onBindViewHolder(@NonNull BookViewHolder holder, int position) {
-        final BookModel book = getItem(position);
-        holder.bookRowItemBinding.setBookModel(book);
-        holder.bookRowItemBinding.executePendingBindings();
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        BookModel book = getItem(position);
+        if (holder instanceof BookViewHolderGrid) {
+            ((BookViewHolderGrid) holder).bookGridItemBinding.setBookModel(book);
+            ((BookViewHolderGrid) holder).bookGridItemBinding.executePendingBindings();
+        } else if (holder instanceof BookViewHolderRow) {
+            ((BookViewHolderRow) holder).bookRowItemBinding.setBookModel(book);
+            ((BookViewHolderRow) holder).bookRowItemBinding.executePendingBindings();
+        }
+
     }
 
-    static class BookViewHolder extends RecyclerView.ViewHolder {
+    static class BookViewHolderRow extends RecyclerView.ViewHolder {
         BookRowItemBinding bookRowItemBinding;
 
-        public BookViewHolder(@NonNull BookRowItemBinding bookRowItemBinding) {
+        public BookViewHolderRow(@NonNull BookRowItemBinding bookRowItemBinding) {
             super(bookRowItemBinding.getRoot());
             this.bookRowItemBinding = bookRowItemBinding;
+        }
+    }
+
+    static class BookViewHolderGrid extends RecyclerView.ViewHolder {
+        BookGridItemBinding bookGridItemBinding;
+
+        public BookViewHolderGrid(@NonNull BookGridItemBinding bookGridItemBinding) {
+            super(bookGridItemBinding.getRoot());
+            this.bookGridItemBinding = bookGridItemBinding;
         }
     }
 }

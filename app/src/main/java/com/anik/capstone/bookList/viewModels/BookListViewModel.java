4454990e33bd84237e2242738d98d516c9b862d0
Ani <ani_tovmasyan@edu.aua.am>
2024-03-17 17:23:10 +0400

    Fix merge conflicts

--- app/src/main/java/com/anik/capstone/bookList/viewModels/BookListViewModel.java
+++ app/src/main/java/com/anik/capstone/bookList/viewModels/BookListViewModel.java
@@ -63,16 +63,4 @@ public class BookListViewModel extends ViewModel {
     public void loadBooks() {
 
     }
-
-    public void onItemViewClick() {
-        if (_layoutViewType.getValue() == GRID) {
-            _layoutViewType.setValue(ROW);
-        } else if (_layoutViewType.getValue() == ROW) {
-            _layoutViewType.setValue(GRID);
-        }
-    }
-
-    public void setButtonIcon(int resId) {
-        _icon.setValue(resourceHelper.getDrawable(resId));
-    }
 }

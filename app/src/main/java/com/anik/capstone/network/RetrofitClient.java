187b7e10b9c522faaae9794193df2eacbb635c7d
Angela Dovanjyan <angeladovanjyan@gmail.com>
2024-05-02 00:36:55 +0400

    Add crashlytics, analytics, and OkHttp logging

--- app/src/main/java/com/anik/capstone/network/RetrofitClient.java
+++ app/src/main/java/com/anik/capstone/network/RetrofitClient.java
@@ -1,5 +1,7 @@
 package com.anik.capstone.network;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -10,8 +12,14 @@ public class RetrofitClient {
 
     public Retrofit getClient() {
         if (retrofit == null) {
+            HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+            OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                    .addInterceptor(logging)
+                    .build();
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
+                    .client(okHttpClient)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
